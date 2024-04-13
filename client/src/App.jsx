7a60c8a4263d6583749adcc850afac05b47fb0a1
cheang120 <babycode.2023@gmail.com>
2024-04-13 23:01:05 +0800

    Header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,13 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Projects from './pages/Projects';
 import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
