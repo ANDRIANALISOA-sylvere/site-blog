5295edf774506964fe85b16afddbc6034bfcd3f1
cheang120 <babycode.2023@gmail.com>
2024-04-16 17:32:58 +0800

    create middleware and a function of handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,14 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req,res) => {
+export const signup = async (req,res,next) => {
     // console.log(req.body);
     const {username,email,password} =req.body
 
     if (!username || !email || !password || username ==='' || email==='' || password === '' ) {
-        return res.status(400).json({message:'All fields are required'})
+        // return res.status(400).json({message:'All fields are required'})
+        next(errorHandler(400,'All fields are required'))
     }
 
     const hashedPassword = bcryptjs.hashSync(password,10)
@@ -20,7 +22,7 @@ export const signup = async (req,res) => {
         await newUser.save()
         res.json('Signup successfull')
     } catch (error) {
-        res.status(500).json({message:error.message})
+        next(error)
     }
 
 
--- api/index.js
+++ api/index.js
@@ -25,3 +25,14 @@ app.listen(3003, ()=> {
 
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
+
+app.use((err,req, res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
