6f414c176445bd78a21959c583e69929f08ff6bb
cheang120 <babycode.2023@gmail.com>
2024-04-17 22:07:01 +0800

    complete footer page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from './pages/SignUp';
 import Projects from './pages/Projects';
 import Dashboard from './pages/Dashboard';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 export default function App() {
@@ -21,6 +22,7 @@ export default function App() {
         <Route path='/projects' element={<Projects />} />
         <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
