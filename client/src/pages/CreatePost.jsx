46433bcab87c152e705d083211d7fa03a9e95281
cheang120 <babycode.2023@gmail.com>
2024-04-26 22:52:50 +0800

    complete create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,6 +18,7 @@ export default function CreatePost() {
     const [imageUploadProgress, setImageUploadProgress] = useState(null);
     const [imageUploadError, setImageUploadError] = useState(null);
     const [formData, setFormData] = useState({});
+    // console.log(formData);
     const [publishError, setPublishError] = useState(null);
   
     const navigate = useNavigate();
@@ -58,12 +59,37 @@ export default function CreatePost() {
           console.log(error);
         }
       };
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const res = await fetch('/api/post/create', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          });
+          const data = await res.json();
+          if (!res.ok) {
+            setPublishError(data.message);
+            return;
+          }
+    
+          if (res.ok) {
+            setPublishError(null);
+            navigate(`/post/${data.slug}`);
+          }
+        } catch (error) {
+          setPublishError('Something went wrong');
+        }
+      };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
       <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
       <form 
         className='flex flex-col gap-4' 
-        // onSubmit={handleSubmit}
+        onSubmit={handleSubmit}
      >
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
           <TextInput
@@ -72,14 +98,14 @@ export default function CreatePost() {
             required
             id='title'
             className='flex-1'
-            // onChange={(e) =>
-            //   setFormData({ ...formData, title: e.target.value })
-            // }
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
           />
           <Select
-            // onChange={(e) =>
-            //   setFormData({ ...formData, category: e.target.value })
-            // }
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
           >
             <option value='uncategorized'>Select a category</option>
             <option value='javascript'>JavaScript</option>
@@ -127,18 +153,18 @@ export default function CreatePost() {
           placeholder='Write something...'
           className='h-72 mb-12'
           required
-        //   onChange={(value) => {
-        //     setFormData({ ...formData, content: value });
-        //   }}
+          onChange={(value) => {
+            setFormData({ ...formData, content: value });
+          }}
         />
         <Button type='submit' gradientDuoTone='purpleToPink'>
           Publish
         </Button>
-        {/* {publishError && (
+        {publishError && (
           <Alert className='mt-5' color='failure'>
             {publishError}
           </Alert>
-        )} */}
+        )}
       </form>
     </div>
   )
