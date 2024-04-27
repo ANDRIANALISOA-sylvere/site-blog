f0ad5396f0c7897d75bfc37fc56d42fbd09b927f
cheang120 <babycode.2023@gmail.com>
2024-04-27 23:06:54 +0800

    add delete post functionality to the dashboard

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -72,17 +72,17 @@ export const getposts = async (req, res, next) => {
   }
 };
 
-// export const deletepost = async (req, res, next) => {
-//   if (!req.user.isAdmin || req.user.id !== req.params.userId) {
-//     return next(errorHandler(403, 'You are not allowed to delete this post'));
-//   }
-//   try {
-//     await Post.findByIdAndDelete(req.params.postId);
-//     res.status(200).json('The post has been deleted');
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+export const deletepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this post'));
+  }
+  try {
+    await Post.findByIdAndDelete(req.params.postId);
+    res.status(200).json('The post has been deleted');
+  } catch (error) {
+    next(error);
+  }
+};
 
 // export const updatepost = async (req, res, next) => {
 //   if (!req.user.isAdmin || req.user.id !== req.params.userId) {
--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -49,6 +49,28 @@ export default function DashPosts() {
       console.log(error.message);
     }
   };
+
+  const handleDeletePost = async () => {
+    setShowModal(false);
+    try {
+      const res = await fetch(
+        `/api/post/deletepost/${postIdToDelete}/${currentUser._id}`,
+        {
+          method: 'DELETE',
+        }
+      );
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        setUserPosts((prev) =>
+          prev.filter((post) => post._id !== postIdToDelete)
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length >0 ? (
@@ -81,7 +103,17 @@ export default function DashPosts() {
                     </Link>
                   </Table.Cell>
                   <Table.Cell>{post.category}</Table.Cell>
-                  <Table.Cell><span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span></Table.Cell>
+                  <Table.Cell>
+                    <span 
+                      className='font-medium text-red-500 hover:underline cursor-pointer'
+                      onClick={() => {
+                        setShowModal(true);
+                        setPostIdToDelete(post._id);
+                      }}
+                    >
+                      Delete
+                    </span>
+                  </Table.Cell>
                   <Table.Cell>
                     <Link className='text-teal-500 hover:underline cursor-pointer' to={`/update-post/${post._id}`}>
                       <span>Edit</span>
@@ -103,6 +135,30 @@ export default function DashPosts() {
       ):(
         <p>You have no posts yet!</p>
       )}
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size='md'
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+              Are you sure you want to delete this post?
+            </h3>
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeletePost}>
+                Yes, I'm sure
+              </Button>
+              <Button color='gray' onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   )
 }
